9368a38d33ffabab6b7bd3cf466fea6d1de085bc
Nick Schimek <cfme00@gmail.com>
2019-09-19 20:07:16 -0700

    Post message when user joins channel

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+require 'http'
 
 get '/' do
   'Hello World'
@@ -7,5 +8,12 @@ end
 
 post '/' do
   data = JSON.parse request.body.read
-  data['challenge']
+
+  rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+    token: ENV['SLACK_API_TOKEN'],
+    channel: '#botleytestchannel',
+    text: 'hello @poppa_nick'
+  })
+  logger.info JSON.pretty_generate(JSON.parse(rc.body))
+  :ok
 end
