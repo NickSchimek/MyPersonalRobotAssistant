e3a8286a957ea992dab2b75d0675bce572ef5579
Nick Schimek <cfme00@gmail.com>
2019-09-19 20:28:48 -0700

    mention poppa_nick

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ post '/' do
   rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
     token: ENV['SLACK_API_TOKEN'],
     channel: '#botleytestchannel',
-    text: 'hello @poppa_nick'
+    text: 'hello <@poppa_nick>'
   })
   logger.info JSON.pretty_generate(JSON.parse(rc.body))
   :ok
