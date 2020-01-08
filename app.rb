3df92687e620ae1a7178b8b3ba27659f40d03a07
Nick Schimek <cfme00@gmail.com>
2020-01-08 11:12:06 -0800

    Update cfPDX record expunge welcome message

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ end
 post '/' do
   data = JSON.parse request.body.read
   user = data['event']['user']
-  text = "Hello <@#{user}>, welcome to the Record Expungement Project!\n\nHead over to https://codeforpdx.github.io/recordexpungPDX/ to get famaliar with the project.\n\nWelcome aboard!"
+  text = "Hello <@#{user}>, welcome to the Record Expungement Project!\n\nHead over to https://github.com/codeforpdx/recordexpungPDX/ to get famaliar with the project.\n\nWelcome aboard!"
   rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
     token: ENV['SLACK_API_TOKEN'],
     channel: '#record_expung',
