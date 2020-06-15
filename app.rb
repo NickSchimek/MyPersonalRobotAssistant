88da42f8a234f35e50dacb113f4803fb6f99ca91
Nick Schimek <cfme00@gmail.com>
2020-06-16 00:02:08 +0900

    Use interpolation

--- app.rb
+++ app.rb
@@ -31,63 +31,63 @@ HEREDOC
 end
 
 def blocks user
-  '[
+  "[
     {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "Hello <@#{user}>, welcome to the Record Expungement Project!"
+      'type': 'section',
+      'text': {
+        'type': 'mrkdwn',
+        'text': 'Hello <@#{user}>, welcome to the Record Expungement Project!'
       }
     },
     {
-      "type": "section",
-      "text": {
-        "type": "plain_text",
-        "text": "Here are some project resources to get started:",
-        "emoji": true
+      'type': 'section',
+      'text': {
+        'type': 'plain_text',
+        'text': 'Here are some project resources to get started:',
+        'emoji': true
       }
     },
     {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "1. Our website: recordsponge.com"
+      'type': 'section',
+      'text': {
+        'type': 'mrkdwn',
+        'text': '1. Our website: recordsponge.com'
       }
     },
     {
-      "type": "image",
-      "title": {
-        "type": "plain_text",
-        "text": "recordsponge.com",
-        "emoji": true
+      'type': 'image',
+      'title': {
+        'type': 'plain_text',
+        'text': 'recordsponge.com',
+        'emoji': true
       },
-      "image_url": "https://res.cloudinary.com/da0cmt1gu/image/upload/t_media_lib_thumb/v1592229029/Screen_Shot_2020-06-15_at_10.46.47_PM_zm5c1l.png",
-      "alt_text": "marg"
+      'image_url': 'https://res.cloudinary.com/da0cmt1gu/image/upload/t_media_lib_thumb/v1592229029/Screen_Shot_2020-06-15_at_10.46.47_PM_zm5c1l.png',
+      'alt_text': 'marg'
     },
     {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "2. Our <https://calendar.google.com/calendar?cid=bGo4aW91MTFwZ2ZlZnE3ZG12MGQ0M2FrcGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ|meetings calendar>"
+      'type': 'section',
+      'text': {
+        'type': 'mrkdwn',
+        'text': '2. Our <https://calendar.google.com/calendar?cid=bGo4aW91MTFwZ2ZlZnE3ZG12MGQ0M2FrcGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ|meetings calendar>'
       }
     },
     {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "3. Our <https://github.com/codeforpdx/recordexpungPDX/|github repo>"
+      'type': 'section',
+      'text': {
+        'type': 'mrkdwn',
+        'text': '3. Our <https://github.com/codeforpdx/recordexpungPDX/|github repo>'
       }
     },
     {
-      "type": "section",
-      "text": {
-        "type": "plain_text",
-        "text": "Welcome aboard!",
-        "emoji": true
+      'type': 'section',
+      'text': {
+        'type': 'plain_text',
+        'text': 'Welcome aboard!',
+        'emoji': true
       }
     },
     {
-      "type": "divider"
+      'type': 'divider'
     }
-  ]'
+  ]"
 end
